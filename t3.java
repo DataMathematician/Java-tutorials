8b5570a419e91f94be8d14acaf5405582ac168fe
DataMathematician <datamathematician@gmail.com>
2020-12-01 22:48:17 +0300

    for
    using for statement

---
+++ t3.java
@@ -0,0 +1,8 @@
+public class t3 {
+    public static void main(String args[]){
+        int count;
+
+        for (count = 0; count < 10; count ++)
+            System.out.println("Counter: " + count);
+    }
+}
