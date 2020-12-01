891b1f157f43fafcb2cf5ebe48f547ff502b2318
DataMathematician <datamathematician@gmail.com>
2020-12-01 22:34:39 +0300

    if
    if ststement usage

---
+++ t2.java
@@ -0,0 +1,15 @@
+public class t2 {
+    public static void main(String args[]){
+        int a,b,c;
+        a = 2;
+        b = 3;
+
+        if (a<b) System.out.println("b > a");
+        if (a==b) System.out.println("b = a");
+        System.out.println();
+        
+        c = b-a;
+        System.out.println("C is: " + c);
+
+    }
+}
