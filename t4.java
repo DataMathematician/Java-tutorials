48e8ad1e4f8c02aa360d47c5f395d62f9aa79073
DataMathematician <datamathematician@gmail.com>
2020-12-01 22:59:35 +0300

    blocks
    use blocks in if statement

---
+++ t4.java
@@ -0,0 +1,14 @@
+public class t4 {
+    public static void main(String args[]){
+        double i,j,d;
+
+        i = 5;
+        j = 10;
+        
+        if (i!=0){
+            System.out.println("i != 0");
+            d = j/i;
+            System.out.print("j/i=" + d);
+        }
+    }
+}
