cadc29aca9af53f335c2259a74c09240e13c06e1
DataMathematician <datamathematician@gmail.com>
2020-12-01 23:27:29 +0300

    v.0.2
    galons to liters

--- tutor.java
+++ tutor.java
@@ -1,12 +1,16 @@
 class tutor{
     public static void main(String args[]){
-        double galons,litres;
-
-        galons = 3.7854;
-        litres = 10;
-
-        litres = galons * litres;
- 
-        System.out.println("Litres to galons: " + litres);
+        double gallons,litres;
+        int c;
+        c = 0;
+        for (gallons = 1; gallons <= 100; gallons++){
+            litres = gallons * 3.7854;
+            System.out.println(gallons + " gallons are " + litres + " litres");
+            c ++;
+            if (c == 10){
+                System.out.println();
+                c = 0;
+            }
+        }
     }
 }
