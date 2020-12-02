9d925de1a7b2e98ac5e4cba2ba4d278e374bc86a
DataMathematician <datamathematician@gmail.com>
2020-12-02 19:22:35 +0300

    Dynamic initialization
    Just dynamic initialization

---
+++ t9.java
@@ -0,0 +1,7 @@
+public class t9 {
+    public static void main(String args[]){
+        double r = 4, height = 5;
+        double rez = r*r*height*3.1416;
+        System.out.println(rez);
+    }    
+}
