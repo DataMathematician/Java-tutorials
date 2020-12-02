f19b15651b25f46422baf92756c24029cff3e3c5
DataMathematician <datamathematician@gmail.com>
2020-12-02 17:59:00 +0300

    Distance
    Distance to object by sound speed

---
+++ t8.java
@@ -0,0 +1,10 @@
+public class t8 {
+    public static void main(String args[]){
+        int speed;
+        double time,rez;
+        speed = 1100;
+        time = 7.2;
+        rez = speed * time/2;
+        System.out.println("Distance is " + rez);
+    }
+}
