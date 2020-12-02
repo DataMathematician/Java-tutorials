69eb7ed9b67d740e6fa9bbfcd3621a45482ac83c
DataMathematician <datamathematician@gmail.com>
2020-12-02 16:56:50 +0300

    ascii/char/ord comparison
    comparison 1 prog in 2 languages

---
+++ t6.java
@@ -0,0 +1,12 @@
+public class t6 {
+    public static void main(String args[]){
+        char ch;
+        ch = 'X';
+        System.out.println("Ch is " + ch);
+        ch ++;
+        System.out.println("Ch is " + ch);
+        ch = 90;
+        System.out.println("Ch is " + ch);
+        
+    }
+}
---
+++ t6.py
@@ -0,0 +1,12 @@
+class t6():
+    def main(self):
+        ch = 'X'
+        print(ch)
+        ch = ord(ch)
+        ch += 1
+        ch = chr(ch)
+        print(ch)
+        ch = chr(90)
+        print(ch)
+
+t6().main()
