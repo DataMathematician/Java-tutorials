dab75a3e144a259eb4393a88543f74f56e0a38b5
DataMathematician <datamathematician@gmail.com>
2020-12-02 17:13:14 +0300

    Logic comparison
    difference java/python
    (no difference)

---
+++ t7.java
@@ -0,0 +1,15 @@
+public class t7 {
+    public static void main(String args[]){
+        boolean b;
+        b = false;
+        System.out.println(b);
+        b = true;
+        System.out.println(b);
+
+        if (b) System.out.println("B is true" + b);
+        b = false;
+        if (b) System.out.println("B is false" + b);
+
+        System.out.println("Logic comparison: " + (10>9));
+    }    
+}
---
+++ t7.py
@@ -0,0 +1,5 @@
+class t7():
+    def main(self):
+        print("Logic comparison: ",10>9) 
+
+t7().main()
