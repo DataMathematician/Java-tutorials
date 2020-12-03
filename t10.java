69895544a4405bdcb34b3440250e952f9b45f97e
DataMathematician <datamathematician@gmail.com>
2020-12-03 17:53:45 +0300

    view areas

---
+++ t10.java
@@ -0,0 +1,17 @@
+public class t10 {
+    public static void main(String args[]){
+        int x;
+        x = 10;
+
+        if (x==10){
+            int y = 20;
+            System.out.println("x and y: " + x + " " + y);
+            x = y*2;
+        }
+        System.out.println("x is " + x );
+        x = 'r';
+        System.out.println("x is " + x );
+        x = 22;
+        System.out.println("x is " + x );
+    }
+}
