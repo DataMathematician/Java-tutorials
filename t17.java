edf542e05d79fc6c315d36963d82e73b7da47120
DataMathematician <datamathematician@gmail.com>
2020-12-04 23:58:03 +0300

    for 100\-100
    example

---
+++ t17.java
@@ -0,0 +1,8 @@
+public class t17 {
+    public static void main(String args[]){
+        double num;
+        for (num = 100;num > -100;num -= 5){
+            System.out.println("Num is " + num);
+        }
+    }
+}
