01c5af1fe5097a1ff4a626ab33165d968cf0c4c4
DataMathematician <datamathematician@gmail.com>
2020-12-04 23:07:01 +0300

    case
    case statement

---
+++ t15.java
@@ -0,0 +1,25 @@
+public class t15 {
+    public static void main(String args[]){
+        int i;
+        for(i = 0;i<10;i++)
+            switch (i) {
+                case 0:
+                    System.out.println(i);
+                    break;
+                case 1:
+                    System.out.println(i);
+                    break;
+                case 2:
+                    System.out.println(i);
+                    break;
+                case 3:
+                    System.out.println(i);
+                    break;
+                case 4:
+                    System.out.println(i);
+                    break;
+                default:
+                    System.out.println(i);
+            }
+       }
+}
