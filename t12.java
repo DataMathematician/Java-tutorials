04b31b1d92d9ecf736b89ab629ee4bd3e4f76d9c
DataMathematician <datamathematician@gmail.com>
2020-12-04 16:27:23 +0300

    IO
    Example input/output

---
+++ t12.java
@@ -0,0 +1,9 @@
+public class t12 {
+    public static void main(String args[])
+        throws java.io.IOException {
+            char ch;
+            System.out.println("Enter button: ");
+            ch = (char) System.in.read();
+            System.out.println("You etered - " + ch);
+    }
+}
