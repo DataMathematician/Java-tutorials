e3b50c44971aefaed629d67d86d9db4da0713880
DataMathematician <datamathematician@gmail.com>
2020-12-05 17:26:10 +0300

    for outer i++
    example

---
+++ t20.java
@@ -0,0 +1,9 @@
+public class t20 {
+    public static void main(String args[]){
+        int i;
+        for (i=0;i<10;){
+            System.out.println("i is" + i);
+            i++;
+        }
+    }
+}
