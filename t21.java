6fac43eedeb66882ed19b9e40605bd911ba8256e
DataMathematician <datamathematician@gmail.com>
2020-12-05 17:32:04 +0300

    for / outer init / outer i++
    example

---
+++ t21.java
@@ -0,0 +1,10 @@
+public class t21{
+    public static void main(String args[]){
+        int i;
+        i = 0;
+        for (; i<10;){
+            System.out.println("i is " + i);
+            i++;
+        }
+    }
+}
