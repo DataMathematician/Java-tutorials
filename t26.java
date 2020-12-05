87c0d86dd9dfde881cf25a8e147b508ca58f11a6
DataMathematician <datamathematician@gmail.com>
2020-12-05 20:29:51 +0300

    do while
    example

---
+++ t26.java
@@ -0,0 +1,10 @@
+public class t26 {
+    public static void main(String args[])
+        throws java.io.IOException {
+            char ch;
+            do{ 
+                System.out.println("Enter button: ");
+                ch = (char) System.in.read();
+            }   while (ch != 'q' );
+    }
+}
