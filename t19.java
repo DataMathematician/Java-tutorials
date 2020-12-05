49cfbc339686c07390c918115cf4addbfeb934cb
DataMathematician <datamathematician@gmail.com>
2020-12-05 17:17:24 +0300

    for with char (bool)
    Broken?

---
+++ t19.java
@@ -0,0 +1,8 @@
+public class t19 {
+    public static void main(String args[])
+        throws java.io.IOException {
+            int i;
+            for (i = 0; (char) System.in.read() != 'S'; i++)
+                System.out.println("i is " + i + '\n');         // выводится 3 раза ???
+    }
+}
