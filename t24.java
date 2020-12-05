57680cf4301c8e0dfef9e4ba1586d8569e620740
DataMathematician <datamathematician@gmail.com>
2020-12-05 20:01:35 +0300

    while
    example

---
+++ t24.java
@@ -0,0 +1,10 @@
+public class t24 {
+    public static void main(String args[]){
+        char ch;
+        ch = 'A';
+        while (ch <= 9999){
+            System.out.println(ch);
+            ch++;
+        }
+    }
+}
