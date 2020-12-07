7b71024e1b50928003045b18663a181e7c64647c
DataMathematician <datamathematician@gmail.com>
2020-12-07 21:27:33 +0300

    0.5

--- Vehicles.java
+++ Vehicles.java
@@ -6,12 +6,18 @@ class Vehicle{
     int range(){
         return fuelcap * mpg;
     }
+    
+    double fuelneeded(int miles){
+        return (double) miles / mpg;
+    }
 }
 
 public class Vehicles {
     public static void main(String args[]){
         Vehicle minivan = new Vehicle();
         Vehicle sportscar = new Vehicle();
+        int dist = 212;
+        double gallons;
         
         minivan.passengers = 8;
         minivan.fuelcap = 50;
@@ -21,7 +27,12 @@ public class Vehicles {
         sportscar.fuelcap = 20;
         sportscar.mpg = 8;
 
+        gallons = minivan.fuelneeded(dist);
         System.out.println("Minivan: " + minivan.passengers + " for range of " + minivan.range());
+        System.out.println("gas in gallons "  + gallons + '\n');
+
+        gallons = sportscar.fuelneeded(dist);
         System.out.println("Minivan: " + sportscar.passengers + " for range of " + sportscar.range());
+        System.out.println("gas in gallons "  + gallons + '\n');
     }
 }
